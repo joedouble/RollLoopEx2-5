424149f078e1a170891263f88bd803e1ec2207c0
Joseph Double <josephdouble@Josephs-MacBook-Pro.local>
2019-01-28 14:30:09 -0500

    Committing code that meets the requirements of the problem.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int counter = 0;
+        int roll = 0;
+        boolean loopCondition = false;
+
+        Random ran = new Random();
+
+        do {
+            roll = ran.nextInt();
+            if (roll != 0) {
+                counter++;
+                loopCondition = true;
+            } else {
+                System.out.println("After " + counter + " rolls, roll is " + roll);
+                loopCondition = false;
+            }
+        } while (loopCondition);
+
     }
 }
